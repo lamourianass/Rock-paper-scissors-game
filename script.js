89092c102a870371f7bca2986ef01d8c6de5de21
Anass Lamouri <anasslamouri@icloud.com>
2025-08-08 13:50:43 +0100

    Create computerPlay() function

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
 
   function btnClicked() {
-    console.log(`You clicked ${button.id}!`)
+    console.log("User choice", button.id)
   }
 
   button.addEventListener('click', btnClicked)
 })
+
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"]
+  const randomChoice = Math.floor(Math.random() * choices.length)
+  return choices[randomChoice]
+}
