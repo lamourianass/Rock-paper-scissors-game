166f03cae24ecca2b8572d131e624c6955c8e5f0
Anass Lamouri <anasslamouri@icloud.com>
2025-08-08 14:21:54 +0100

    Create playRound() function

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const buttons = document.querySelectorAll('button')
 
 buttons.forEach(button => {
 
-  function btnClicked() {
-    console.log("User choice", button.id)
-  }
 
-  button.addEventListener('click', btnClicked)
+  button.addEventListener('click', () => {
+    playRound(button.id, computerPlay())
+  })
 })
 
 function computerPlay() {
@@ -15,3 +14,18 @@ function computerPlay() {
   const randomChoice = Math.floor(Math.random() * choices.length)
   return choices[randomChoice]
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
